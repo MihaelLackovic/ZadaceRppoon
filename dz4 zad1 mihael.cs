b8df89723e492308d18f2a30d2e2d4488bb5a385
MihaelLackovic <156356873+MihaelLackovic@users.noreply.github.com>
2024-01-29 17:03:09 +0100

    Add files via upload

---
+++ dz4 zad1 mihael.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+abstract class Car
+{
+    public abstract void Drive();
+}
+
+public class Ford : Car
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Driving a Ford");
+    }
+}
+
+public class Toyota : Car
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Driving a Toyota");
+    }
+}
+
+abstract class CarFactory
+{
+    public abstract Car CreateCar();
+}
+
+public class FordFactory : CarFactory
+{
+    public override Car CreateCar()
+    {
+        return new Ford();
+    }
+}
+
+public class ToyotaFactory : CarFactory
+{
+    public override Car CreateCar()
+    {
+        return new Toyota();
+    }
+}
+class Client
+{
+    public void TestDrive(CarFactory carFactory)
+    {
+        Car car = carFactory.CreateCar();
+        car.Drive();
+    }
+
+    static void Main()
+    {
+        Client client = new Client();
+
+        CarFactory fordFactory = new FordFactory();
+        client.TestDrive(fordFactory);  
+
+        CarFactory toyotaFactory = new ToyotaFactory();
+        client.TestDrive(toyotaFactory); 
+    }
---
+++ dz5  mihael.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+Primjeni Dekorater na ovaj set klasa:
+namespace DekoraterExtraZadatak
+{
+    public interface IMeal
+    {
+        void AddIngredient();
+    }
+    public class Noodles:IMeal
+    {
+        public void AddIngredient()
+        {
+            Console.WriteLine("Add Noodles");
+        }
+    }
+
+    public class Beef: IMeal
+    {
+        public void AddIngredient()
+        {
+            Console.WriteLine("Add Beef");
+        }
+    }
+
+    public class Mushrooms: IMeal
+    {
+        public void AddIngredient()
+        {
+            Console.WriteLine("Add Mushrooms");
+        }
+    }
+
+    public class Water: IMeal
+    {
+        public void AddIngredient()
+        {
+            Console.WriteLine("Add Water");
+        }
+    }
+    public class Decorator : IMeal
+    {
+        private readonly IMeal imeal;
+
+        public SpiceDecorator(IMeal mealComponent)
+        {
+            imeal = mealComponent;
+        }
+
+        public void AddIngredient()
+        {
+            imeal.AddIngredient();
+            Console.WriteLine("Add Spice");
+        }
+    }
+    public class NoodlesDecorator : Decorator
+    {
+        public NoodlesDecorator(IMeal mealComponent) : base(mealComponent) { }
+    }
+
+    public class BeefDecorator : Decorator
+    {
+        public BeefDecorator(IMeal mealComponent) : base(mealComponent) { }
+    }
+
+    public class MushroomsDecorator : Decorator
+    {
+        public MushroomsDecorator(IMeal mealComponent) : base(mealComponent) { }
+    }
+
+    public class WaterDecorator : SpiceDecorator
+    {
+        public WaterDecorator(IMeal mealComponent) : base(mealComponent) { }
+    }
+
+    public class Meal
+    {
+        Beef beef;
+        Mushrooms mushrooms;
+        Noodles noodles;
+        Water water;
+        public Meal()
+        {
+            beef = new Beef();
+            mushrooms = new Mushrooms();
+            noodles = new Noodles();
+            water = new Water();
+        }
+        public void MakeMushroomNoodleSoup()
+        {
+            water.AddWater();
+            mushrooms.AddMushrooms();
+            noodles.AddNoddles();
+        }
+
+        public void MakeBeefNoodleSoup()
+        {
+            water.AddWater();
+            beef.AddBeef();
+            noodles.AddNoddles();
+        }
+
+        public void MakeMushroomBeefSoup()
+        {
+            water.AddWater();
+            beef.AddBeef();
+            mushrooms.AddMushrooms();
+        }
+    }
+
+    public static class ClientCode
+    {
+        public static void Run()
+        {
+            IMeal meal = new Meal();
+            meal.AddIngredient();
+
+            Console.WriteLine();
+
+            IMeal noodles = new NoodlesDecorator(new Noodles());
+            noodles.AddIngredient();
+
+            Console.WriteLine();
+
+            IMeal beefMushrooms = new MushroomsDecorator(new BeefDecorator(new Meal()));
+            beefMushrooms.AddIngredient();
+        }
+    }
+}
